da3004960c3b6a577cc5deed0fb2c7d119aaecb5
Aver roes <tamerabdalrazaq@hotmail.com>
2024-12-15 17:46:52 +0200

    fix

--- hw3secws.c
+++ hw3secws.c
@@ -266,6 +266,7 @@ static unsigned int comp_packet_to_rules(struct sk_buff *skb, const struct nf_ho
                 log_entry.reason = REASON_NO_MATCHING_RULE;   
             else              
                 log_entry.reason = i;   
+            print_packet_logs();
             return rule->action; // Return the matching rule's action
         }
     } else{
