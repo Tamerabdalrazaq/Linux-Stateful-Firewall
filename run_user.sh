3b2076e8707e68382d0ef726e625c8f4e75a9491
Aver roes <tamerabdalrazaq@hotmail.com>
2025-01-01 15:39:59 +0200

    add a new connectino

--- run_user.sh
+++ run_user.sh
@@ -2,4 +2,4 @@ sudo mknod /dev/fw_log c 243 0
 python3 ./user/main.py load_rules rules
 python3 ./user/main.py show_rules
 python3 ./user/main.py show_log
-cat python3 ./user/main.py show_log
+python3 ./user/main.py show_log
