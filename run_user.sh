323ea44f062c9313d9a5ddecfab73b373d9d8304
Aver roes <tamerabdalrazaq@hotmail.com>
2025-01-06 10:24:43 +0200

    TCP State Machine implementation ---- device sleep bug

--- run_user.sh
+++ run_user.sh
@@ -1,4 +1,8 @@
 sudo mknod /dev/fw_log c 243 0
+echo "Loading Rules.."
 sudo python3 ./user/main.py load_rules rules
 sudo python3 ./user/main.py show_rules
+echo "Current Logs:"
 sudo python3 ./user/main.py show_log
+echo "Connections Table .."
+sudo python3 ./user/main.py show_conns
