1d7442180d32bf468e8acc14ee46f38680bd860e
Aver roes <tamerabdalrazaq@hotmail.com>
2025-01-14 19:41:15 +0200

    MITM 800 - testing dynamically finding cli and srv addresses

--- module/firewall.c
+++ module/firewall.c
@@ -1308,7 +1308,7 @@ static int handle_mitm_local_out(struct sk_buff *skb, packet_identifier_t* origi
     // •	Server-to-client, outbound, local-out,
     else { 
         original_port = htons(80);
-        original_ip = (original_packet_identifier.dst_ip);
+        original_ip = (original_packet_identifier->dst_ip);
         int ret = modify_packet(skb, original_ip, original_port, NULL, NULL);
     }
     return 0;
