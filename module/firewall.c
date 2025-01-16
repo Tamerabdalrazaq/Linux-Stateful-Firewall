899994c5851013c4989e38888619c4cdcaecd52a
Aver roes <tamerabdalrazaq@hotmail.com>
2025-01-16 18:47:26 +0200

    Switching branch

--- module/firewall.c
+++ module/firewall.c
@@ -1599,7 +1599,8 @@ static struct file_operations fops = {
 // Initialization function; handles error registering the hooks with cleanups and an indicative return value
 static int __init fw_init(void) {    
     int ret;
-    printk(KERN_INFO "\n\n\n\n\nLoading firewall module...!\n\n\n\n\n");
+    printk(KERN_INFO "\n\n\n\n\nLoading firewall module...!\n");
+    printk(KERN_INFO " __TESTING_BRANCH__\n\n\n\n\n");
     // ******
     // Devices setup
     // ******
