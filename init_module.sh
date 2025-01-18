668bc77b9a39eefbf7fb0be3f4477d323f871afa
Aver roes <tamerabdalrazaq@hotmail.com>
2025-01-18 20:42:47 +0200

    TCP State machine simultaneuous closing stable

--- init_module.sh
+++ init_module.sh
@@ -3,9 +3,9 @@ cd module;
 make;
 sudo insmod firewall.ko;
 cd ..;
-sudo python3 ./http/http_mitm.py > http_output.log 2>&1 &
+sudo python3 ./http/http_mitm.py > http_output.log 2>/dev/tty &
 echo "HTTP proxy activated"
-sudo python3 ./ftp/ftp_mitm.py > ftp_output.log 2>&1 &
+sudo python3 ./ftp/ftp_mitm.py > ftp_output.log 2>/dev/tty &
 echo "FTP proxy activated"
 echo ""
 bash run_user.sh;
