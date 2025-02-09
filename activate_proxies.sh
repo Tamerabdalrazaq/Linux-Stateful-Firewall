151a6a07565961ed11953871901dbc9a57881be5
Aver roes <tamerabdalrazaq@hotmail.com>
2025-02-09 18:14:58 +0200

    Proxies script all

--- activate_proxies.sh
+++ activate_proxies.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
 cd proxies;
-if [[ "$1" == "http" || "$1" == "all" ]]; then
+if [[ "$1" == "http" ]]; then
     cd http;
     sudo python3 http_mitm.py && echo "HTTP proxy activated";
     cd ..;
-elif [[ "$1" == "ftp" || "$1" == "all" ]]; then
+elif [[ "$1" == "ftp" ]]; then
     cd ftp;
     sudo python3 ftp_mitm.py && echo "FTP proxy activated";
     cd ..;
-elif [[ "$1" == "smtp" || "$1" == "all" ]]; then
+elif [[ "$1" == "smtp" ]]; then
     cd smtp;
     sudo python3 smtp_mitm.py && echo "SMTP proxy activated";
     cd ..;
+
+elif [[ "$1" == "all" ]]; then
+    cd http;
+    echo "Activating HTTP proxy";
+    sudo python3 http_mitm.py >/dev/tty 2>/dev/tty &
+    cd ..;
+    cd ftp;
+    echo "Activating ftp proxy";
+    sudo python3 ftp_mitm.py >/dev/tty 2>/dev/tty &
+    cd ..;
+    cd smtp;
+    echo "Activating smtp proxy";
+    sudo python3 smtp_mitm.py >/dev/tty 2>/dev/tty &
+    cd ..;
+    
 else 
     echo "Invalid argument. Use: \n bash activate_proxies.sh ['http', 'ftp', 'smtp', 'all']";
 fi
