72feabb9edada9c2ece386f1c6bf3e10deb2997c
Aver roes <tamerabdalrazaq@hotmail.com>
2025-07-16 15:11:32 +0300

    modular

--- module/helpers.c
+++ module/helpers.c
@@ -1,11 +1,6 @@
 #include <linux/slab.h>    // for kmalloc and kfree
 #include "helpers.h"
 
-char* print_hi(void) {
-    char *msg = kmalloc(48, GFP_KERNEL);
-    if (!msg)
-        return NULL;
-
-    strcpy(msg, "HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
-    return msg;
+int print_hi(void) {
+    return 5;
 }
--- module/helpers.h
+++ module/helpers.h
@@ -2,6 +2,6 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
-char* print_hi(void);
+int print_hi(void);
 
 #endif
